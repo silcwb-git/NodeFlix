43eed605f79f731800f6a6890332b46f6320d500
silcwb <silvana.siqueira@gmail.com>
2023-08-26 19:48:19 -0300

    created POST method

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ app.use(express.json());
 const genres = [
     { id: 1, name: 'Science Fiction' },
     { id: 2, name: 'Comedy' },
-    { id: 3, name: 'Romance' }
+    { id: 3, name: 'Romance' },
+    { id: 4, name: 'Action' }
 ]
 
 app.get('/api/genres', (req, res) => {
@@ -19,6 +20,26 @@ app.get('/api/genres/:id', (req, res) => {
     if (!genre) return res.status(404).send('The genre with the given ID was not found');
     res.send(genre);
 });
+
+app.post('/api/genres/:id', (req, res) => {
+    const { error } = ValidateGenre(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const genre = {
+        id: genres.length + 1,
+        name: req.body.name
+    }
+    genres.push(genre);
+    res.send(genre);
+
+});
+
+function ValidateGenre(genre) {
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+    return Joi.validate(genre), schema;
+}
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
