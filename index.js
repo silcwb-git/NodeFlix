0e1a486568cbdb0241031967c5e27ae9c8afe4ed
silcwb <silvana.siqueira@gmail.com>
2023-08-26 18:37:40 -0300

    crested fists RESTful APIs methods for  genres srv

--- index.js
+++ index.js
@@ -14,5 +14,11 @@ app.get('/api/genres', (req, res) => {
     res.send(genres);
 });
 
+app.get('/api/genres/:id', (req, res) => {
+    const genre = genres.find(g => g.id === parseInt(req.params.id));
+    if (!genre) return res.status(404).send('The genre with the given ID was not found');
+    res.send(genre);
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
