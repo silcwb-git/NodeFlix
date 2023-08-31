3a58d41598a0db4c59f2945ef82571f5ccb1d138
silcwb <silvana.siqueira@gmail.com>
2023-08-30 22:29:07 -0300

    updated file

--- routes/genres.js
+++ routes/genres.js
@@ -49,7 +49,7 @@ router.put('/:id', async (req, res) => {
 router.delete('/:id', async (req, res) => {
     const genre = await Genre.findByIdAndRemove(req.params.id)
     if (!genre) return res.status(404).send('The genre with the given ID was not found');
-    const index = genres.indexOf(genre);
+    //const index = genres.indexOf(genre);
 });
 
 function ValidateGenre(genre) {
