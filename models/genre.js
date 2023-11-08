707c298a14e4816e8f6c16d64b82a3b5becb1341
silcwb <silvana.siqueira@gmail.com>
2023-11-08 20:27:49 -0300

    updated files

--- models/genre.js
+++ models/genre.js
@@ -1,25 +1,25 @@
 const Joi = require('joi');
 const mongoose = require('mongoose');
 
-const genreSchema = mongoose.model('Genre', new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        minLengh: 5,
-        maxLengh: 50
-    }
-}));
+const genreSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 50
+  }
+});
 
 const Genre = mongoose.model('Genre', genreSchema);
 
 function validateGenre(genre) {
-    const schema = {
-        name: Joi.string().min(3).required()
-    };
-    
-    return Joi.validate(genre, schema);
+  const schema = {
+    name: Joi.string().min(3).required()
+  };
+
+  return Joi.validate(genre, schema);
 }
 
 exports.genreSchema = genreSchema;
-exports.Genre = Genre;
+exports.Genre = Genre; 
 exports.validate = validateGenre;
