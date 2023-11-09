3c74a7e1b852633138a6ccf23513a89b2e84e6fc
silcwb <silvana.siqueira@gmail.com>
2023-11-09 00:58:25 -0300

    updated file

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 const mongoose = require('mongoose');
-const express = require('express');
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
-
-mongoose.connect('mongodb://localhost/nodeflix')
-    .then(() => console.log('Connected to MongoDB...'))
-    .catch(err => console.error('Could not connect to MongoDB...', err.message));
-
-// Build a web server
+const movies = require('./routes/movies');
+const express = require('express');
 const app = express();
-app.use(express.json()); // middleware function that sets a req.body property
 
+mongoose.connect('mongodb://localhost/vidly')
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...'));
+
+app.use(express.json());
 app.use('/api/genres', genres);
 app.use('/api/customers', customers);
+app.use('/api/movies', movies);
 
-// Reading the port from an environment variable 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
