bee0aa252d6290b40c1bc755e61079ae2da466d4
silcwb <silvana.siqueira@gmail.com>
2023-11-13 11:34:33 -0300

    updated file

--- models/movie.js
+++ models/movie.js
@@ -2,7 +2,7 @@ const Joi = require('joi');
 const mongoose = require('mongoose');
 const {genreSchema} = require('./genre');
 
-const Movie = mongoose.model('Movies', new mongoose.Schema({
+const Movie = mongoose.model('Movie', new mongoose.Schema({
   title: {
     type: String,
     required: true,
