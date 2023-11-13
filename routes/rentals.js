77d2a2de3da5a4cfd250cf4c8ece69824997a9e0
silcwb <silvana.siqueira@gmail.com>
2023-11-13 11:29:18 -0300

    added post method rto rentals API

--- routes/rentals.js
+++ routes/rentals.js
@@ -1,17 +1,19 @@
-const {Rental, validate} = require('../models/rental');
+const { Rental, validate } = require('../models/rental');
+const { Movie } = require('../models/movie');
+const { Customer } = require('../models/customer');
 const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
 // using route handler functions (req, res) => {}
 router.get('/', async (req, res) => {
-    const rentals = await Rental.find().sort('name');
+    const rentals = await Rental.find().sort('-dateOut');
     res.send(rentals);
 });
 
-router.get('/:id', async(req, res) => {
+router.get('/:id', async (req, res) => {
     const rental = await Rental.findById(req.params.id);
-  
+
     if (!rental) return res.status(404).send('The rental with the given ID was not found');
     res.send(rental);
 });
@@ -20,8 +22,29 @@ router.post('/', async (req, res) => {
     const { error } = validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
-    let rental = new Genre({ name: req.body.name })
+    const customer = await Customer.findById(req.body.customerId);
+    if (!customer) return res.status(400).send('Invalid customer.');
+
+    const movie = await Movie.findById(req.body.movieId);
+    if (!movie) return res.status(400).send('Invalid movie.');
+
+    if (movie.numberInStock === 0) return res.status(400).send('Movie not in stock.');
+
+    let rental = new Rental({
+        customer: {
+            _id: customer._id,
+            name: customer.name,
+            phone: customer.phone
+        },
+        movie: {
+            _id: movie._id,
+            title: movie.title,
+            dailyRentalRate: movie.dailyRentalRate
+        }
+    });
     rental = await rental.save();
+    movie.numberInStock--;
+    movie.save();
 
     res.send(rental);
 });
